3e8ef813362f3c7205626fb8ce3f7177c0db6a18
gurkanmaral <gurkanmaral1@gmail.com>
2023-01-04 14:10:45 +0300

    first commit

---
+++ vite-project/src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from './Navbar'
+import Billing from './Billing'
+import Button from './Button'
+import CardDeal from './CardDeal'
+import Clients from './Clients'
+import Business from './Business'
+import CTA from './CTA'
+import FeedbackCard from './FeedbackCard'
+import Footer from './Footer'
+import GetStarted from './GetStarted'
+import Hero from './Hero'
+import Stats from './Stats'
+import Testimonials from './Testimonials';
+
+export{
+    Navbar,
+    Billing,
+    Business,
+    Button,
+    CardDeal,
+    Clients,
+    CTA,
+    FeedbackCard,
+    Footer,
+    GetStarted,
+    Hero,
+    Stats,
+    Testimonials
+};
